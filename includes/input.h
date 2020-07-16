95885111aacb981ba9abb604c06fbdf4a1238943
anclarma <anclarma@student.42.fr>
2020-07-16 23:16:46 +0200

    maj input.h

--- includes/input.h
+++ includes/input.h
@@ -13,14 +13,25 @@
 #ifndef INPUT_H
 # define INPUT_H
 
+/*
 # define UP_KEY		126
 # define DOWN_KEY	125
 # define LEFT_KEY	123
 # define RIGHT_KEY	124
-# define Z_KEY
-# define S_KEY
+# define Z_KEY		0
+# define S_KEY		0
 # define R_KEY		15
 # define ESC_KEY	53
+*/
+
+# define UP_KEY		65362
+# define DOWN_KEY	65364
+# define LEFT_KEY	65361
+# define RIGHT_KEY	65363
+# define Z_KEY		122
+# define S_KEY		115
+# define R_KEY		114
+# define ESC_KEY	65307
 
 int	deal_mouse(int button, int x, int y, void *param);
 int	deal_key(int key, void *param);
