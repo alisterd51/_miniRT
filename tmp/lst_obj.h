58ae3cc8ce2b14deb3a714c8277e847b8fc433ef
Antoine CLARMAN <anclarma@e1r1p12.42.fr>
2020-10-20 15:46:27 +0200

    test: forret de ifs a couper

--- tmp/lst_obj.h
+++ tmp/lst_obj.h
@@ -6,7 +6,7 @@
 /*   By: anclarma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 14:21:46 by anclarma          #+#    #+#             */
-/*   Updated: 2020/10/19 15:30:03 by anclarma         ###   ########.fr       */
+/*   Updated: 2020/10/20 15:45:28 by anclarma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,43 +23,71 @@ void	print_obj(t_obj *obj);
 void	free_obj(t_obj *obj);
 
 /*
+** res.c
+*/
+void	init_res(char *line, t_obj *obj);
+
+/*
+** lst_amb_light.c
+*/
+void	init_amb_light(char *line, t_obj *obj);
+
+/*
 ** lst_cam.c
 */
+void	init_lst_cam(char *line, t_obj *obj);
 void	free_lst_cam(t_cam *lst_cam);
 
 /*
 ** lst_light.c
 */
+void	init_lst_light(char *line, t_obj *obj);
 void	free_lst_light(t_light *lst_light);
 
 /*
 ** lst_sphere.c
 */
+void	init_lst_sphere(char *line, t_obj *obj);
 void	free_lst_sphere(t_sphere *lst_sphere);
 
 /*
 ** lst_plane.c
 */
+void	init_lst_plane(char *line, t_obj *obj);
 void	free_lst_plane(t_plane *lst_plane);
 
 /*
 ** lst_quare.c
 */
+void	init_lst_square(char *line, t_obj *obj);
 void	free_lst_square(t_square *lst_square);
 
 /*
 ** lst_cylinder.c
 */
+void	init_lst_cylinder(char *line, t_obj *obj);
 void	free_lst_cylinder(t_cylinder *lst_cylinder);
 
 /*
 ** lst_triangle.c
 */
+void	init_lst_triangle(char *line, t_obj *obj);
 void	free_lst_triangle(t_triangle *lst_triangle);
+
+/*
+** lst_cube.c
+*/
+void	init_lst_cube(char *line, t_obj *obj);
+
+/*
+** lst_cube.c
+*/
+void	init_lst_pyramid(char *line, t_obj *obj);
 
 /*
 ** lst_cone.c
 */
+void	init_lst_cone(char *line, t_obj *obj);
 void	free_lst_cone(t_cone *lst_cone);
 
 #endif
--- tmp/parsing.c
+++ tmp/parsing.c
@@ -6,7 +6,7 @@
 /*   By: anclarma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 17:06:45 by anclarma          #+#    #+#             */
-/*   Updated: 2020/10/19 17:29:43 by anclarma         ###   ########.fr       */
+/*   Updated: 2020/10/20 15:45:26 by anclarma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,40 @@
 
 static void	parse_line(char *line, t_obj *obj)
 {
-	
+	if (!line || !*line)
+		return exit_errcode(ERROR_LINE);
+	else if (*line == '#')
+		return ;
+	else if (!*(line + 1))
+		return exit_errcode(ERROR_LINE);
+	else if (*line == 'R' && ft_isspace(*(line + 1)))
+		init_res(line + 1, obj);
+	else if (*line == 'A' && ft_isspace(*(line + 1)))
+		init_amb_light(line + 1, obj);
+	else if (*line == 'c' && ft_isspace(*(line + 1)))
+		init_lst_cam(line + 1, obj);
+	else if (*line == 'l' && ft_isspace(*(line + 1)))
+		init_lst_light(line + 1, obj);
+	else if (!*(line + 2))
+		return exit_errcode(ERROR_LINE);
+	else if (*line == 's' && *(line + 1) == 'p' && ft_isspace(*(line + 2)))
+		init_lst_sphere(line + 1, obj);
+	else if (*line == 'p' && *(line + 1) == 'l' && ft_isspace(*(line + 2)))
+		init_lst_plane(line + 1, obj);
+	else if (*line == 's' && *(line + 1) == 'q' && ft_isspace(*(line + 2)))
+		init_lst_square(line + 1, obj);
+	else if (*line == 'c' && *(line + 1) == 'y' && ft_isspace(*(line + 2)))
+		init_lst_cylinder(line + 1, obj);
+	else if (*line == 't' && *(line + 1) == 'r' && ft_isspace(*(line + 2)))
+		init_lst_triangle(line + 1, obj);
+	else if (*line == 'c' && *(line + 1) == 'u' && ft_isspace(*(line + 2)))
+		init_lst_cube(line + 1, obj);
+	else if (*line == 'p' && *(line + 1) == 'y' && ft_isspace(*(line + 2)))
+		init_lst_pyramid(line + 1, obj);
+	else if (*line == 'c' && *(line + 1) == 'o' && ft_isspace(*(line + 2)))
+		init_lst_cone(line + 1, obj);
+	else
+		return exit_errcode(UNRECOGNIZED_LINE);
 }
 
 void		parsing(char *param, t_obj *obj)
@@ -26,18 +59,18 @@ void		parsing(char *param, t_obj *obj)
 	char	*line;
 
 	if (check_file_rt(param))
-		exit_errcode(NOT_RT_FILE);
+		return (exit_errcode(NOT_RT_FILE));
 	if ((fd = open(param, O_RDONLY)) == -1)
-		exit_errcode(OPEN_ERROR);
+		return (exit_errcode(OPEN_ERROR));
 	while ((ret_gnl = get_next_line(fd, &line)) == 1)
 	{
 		parse_line(line, obj);
 		free(line);
 	}
 	if (ret_gnl == -1)
-		exit_errcode(GNL_ERROR);
+		return (exit_errcode(GNL_ERROR));
 	parse_line(line, obj);
 	free(line);
 	if (close(fd))
-		exit_errcode(CLOSE_ERROR);
+		return (exit_errcode(CLOSE_ERROR));
 }
