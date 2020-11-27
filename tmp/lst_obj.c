e84905f9596f17a42d783c718ccc0e6f6e22b440
anclarma <anclarma@student.42.fr>
2020-11-27 15:45:00 +0100

    test en cours

--- tmp/lst_obj.c
+++ tmp/lst_obj.c
@@ -6,7 +6,7 @@
 /*   By: anclarma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 14:55:07 by anclarma          #+#    #+#             */
-/*   Updated: 2020/11/27 14:25:54 by antoine          ###   ########.fr       */
+/*   Updated: 2020/11/27 15:36:51 by antoine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,12 +55,14 @@ void	free_obj(t_obj **obj)
 //fonction de debug, a retirer avant le rendu
 void	print_res(t_res *res)
 {
-	printf("res: x_size = %d, y_size = %d\n", res->x_size, res->y_size);
+	if (res)
+		printf("res: x_size = %d, y_size = %d\n", res->x_size, res->y_size);
 }
 
 void	print_amb_light(t_amb_light *amb_light)
 {
-	printf("amb_light: ratio = %.1f, color = %hhu,%hhu,%hhu\n", amb_light->ratio, amb_light->color.r, amb_light->color.g, amb_light->color.b);
+	if (amb_light)
+		printf("amb_light: ratio = %.1f, color = %hhu,%hhu,%hhu\n", amb_light->ratio, amb_light->color.r, amb_light->color.g, amb_light->color.b);
 }
 
 void	print_lst_cam(t_cam *cam)
@@ -113,6 +115,60 @@ void	print_lst_plane(t_plane *plane)
 	}
 }
 
+void	print_lst_square(t_square *square)
+{
+	while (square)
+	{
+		printf("square: coord = %.1f,%.1f,%.1f", square->coord.x, square->coord.y, square->coord.z);
+		printf(", normal = %.1f,%.1f,%.1f", square->normal.x, square->normal.y, square->normal.z);	
+		printf(", side = %.1f", square->side);
+		printf(", color = %hhu,%hhu,%hhu", square->color.r, square->color.g, square->color.b);
+		printf(", next = %p\n", square->next);
+		square = square->next;
+	}
+}
+
+void	print_lst_cylinder(t_cylinder *cylinder)
+{
+	while (cylinder)
+	{
+		printf("cylinder: coord = %.1f,%.1f,%.1f", cylinder->coord.x, cylinder->coord.y, cylinder->coord.z);
+		printf(", normal = %.1f,%.1f,%.1f", cylinder->normal.x, cylinder->normal.y, cylinder->normal.z);
+		printf(", color = %hhu,%hhu,%hhu", cylinder->color.r, cylinder->color.g, cylinder->color.b);
+		printf(", diameter = %.1f", cylinder->diameter);
+		printf(", height = %.1f", cylinder->height);
+		printf(", next = %p\n", cylinder->next);
+		cylinder = cylinder->next;
+	}
+}
+
+void	print_lst_triangle(t_triangle *triangle)
+{
+	while (triangle)
+	{
+		printf("triangle: coord1 = %.1f,%.1f,%.1f", triangle->coord1.x, triangle->coord1.y, triangle->coord1.z);
+		printf("triangle: coord2 = %.1f,%.1f,%.1f", triangle->coord2.x, triangle->coord2.y, triangle->coord2.z);
+		printf("triangle: coord3 = %.1f,%.1f,%.1f", triangle->coord3.x, triangle->coord3.y, triangle->coord3.z);
+		printf(", color = %hhu,%hhu,%hhu", triangle->color.r, triangle->color.g, triangle->color.b);
+		printf(", next = %p\n", triangle->next);
+		triangle = triangle->next;
+	}
+}
+
+void	print_lst_cone(t_cone *cone)
+{
+	while (cone)
+	{
+		printf("cone: coord = %.1f,%.1f,%.1f", cone->coord.x, cone->coord.y, cone->coord.z);
+		printf(", normal = %.1f,%.1f,%.1f", cone->normal.x, cone->normal.y, cone->normal.z);
+		printf(", diameter = %.1f", cone->diameter);
+		printf(", height = %.1f", cone->height);
+		printf(", color = %hhu,%hhu,%hhu", cone->color.r, cone->color.g, cone->color.b);
+		printf(", next = %p\n", cone->next);
+		cone = cone->next;
+	}
+}
+
 void	print_obj(t_obj *obj)
 {
 	print_res(obj->res);
@@ -121,8 +177,8 @@ void	print_obj(t_obj *obj)
 	print_lst_light(obj->lst_light);
 	print_lst_sphere(obj->lst_sphere);
 	print_lst_plane(obj->lst_plane);
-//	print_lst_square(obj->lst_square);
-//	print_lst_cylinder(obj->lst_cylinder);
-//	print_lst_triangle(obj->lst_triangle);
-//	print_lst_cone(obj->lst_cone);
+	print_lst_square(obj->lst_square);
+	print_lst_cylinder(obj->lst_cylinder);
+	print_lst_triangle(obj->lst_triangle);
+	print_lst_cone(obj->lst_cone);
 }
